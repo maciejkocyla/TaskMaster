50679cd6efa12aa902ab0956ffbfaffee8d7c3bf
Maciej Kocyła <maciej.kocyla@gmail.com>
2013-01-27 22:14:25 +0100

    user interface in a nutshell

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 TaskMaster::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
+  match 'signout',	to: 'sessions#destroy', via: :delete
+  match 'signin',	to: 'sessions#new'
   match '/signup',	to: 'users#new'
   match '/help',	to: 'static_pages#help'
   match '/about',	to: 'static_pages#about'
